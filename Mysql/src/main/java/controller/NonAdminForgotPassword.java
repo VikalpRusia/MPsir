7bd8cb21316c6e45f0bf7c0ce09119bc8a8768a2
vikalp <vikalprusia@gmail.com>
2021-02-13 17:25:26 +0530

    import optimised

--- Mysql/src/main/java/controller/NonAdminForgotPassword.java
+++ Mysql/src/main/java/controller/NonAdminForgotPassword.java
@@ -19,9 +19,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.Scanner;
